84d1c79c0ea33f2793ec43eaf17c6c15ed6e72bc
lucieg <>
2011-03-24 15:09:52 +0000

    This commit was manufactured by cvs2svn to create tag 'V23-03-11'.
    Sprout from master 2011-03-24 15:09:51 UTC lucieg '*** empty log message ***'
    Delete:
        METsWithPU/METsAnalyzer/python/sources/source_QCD_15_500_7TeV_MCStartupFnal_cff.py
        ThreePhotonSearch/MassPlots/BuildFile
        ThreePhotonSearch/MassPlots/interface/PhotonPlots.h
        ThreePhotonSearch/MassPlots/python/photonplots_cfi.py
        ThreePhotonSearch/MassPlots/src/PhotonPlots.cc
        ThreePhotonSearch/MassPlots/test/photonanalyzerMC_cfg.py
        ThreePhotonSearch/MassPlots/test/photonanalyzer_cfg.py
        Trigger/5e30dev/hltmenu_5e30_r138921_DS_AlCa.cfg
        Trigger/5e30dev/hltmenu_5e30_r138921_DS_Comm_v1.cfg
        Trigger/5e30dev/hltmenu_5e30_r138921_DS_Cosmics_v1.cfg
        Trigger/5e30dev/hltmenu_5e30_r138921_DS_EGMon_v1.cfg
        Trigger/5e30dev/hltmenu_5e30_r138921_DS_JMTMon_v1.cfg
        Trigger/5e30dev/hltmenu_5e30_r138921_DS_JetMet_v1.cfg
        Trigger/5e30dev/hltmenu_5e30_r138921_DS_MinBias_v1.cfg
        Trigger/5e30dev/hltmenu_5e30_r138921_DS_MuMon.cfg
        Trigger/5e30dev/hltmenu_5e30_r138921_DS_MuOnia_v1.cfg
        Trigger/5e30dev/hltmenu_5e30_r138921_DS_Tau_v1.cfg
        Trigger/ratesLumiFit/Datasets_RatesLumiFit.list
        Trigger/ratesLumiFit/GetIndividualPerLSSlices.C
        Trigger/ratesLumiFit/GetSlopesInterceptsMultRuns.C
        Trigger/ratesLumiFit/OHltConfig.cpp
        Trigger/ratesLumiFit/OHltConfig.h
        Trigger/ratesLumiFit/OHltEffPrinter.cpp
        Trigger/ratesLumiFit/OHltEffPrinter.h
        Trigger/ratesLumiFit/OHltMenu.cpp
        Trigger/ratesLumiFit/OHltMenu.h
        Trigger/ratesLumiFit/OHltRateCounter.cpp
        Trigger/ratesLumiFit/OHltRateCounter.h
        Trigger/ratesLumiFit/OHltRateEff
        Trigger/ratesLumiFit/OHltRateEff.cpp
        Trigger/ratesLumiFit/OHltRatePrinter.cpp
        Trigger/ratesLumiFit/OHltRatePrinter.h
        Trigger/ratesLumiFit/OHltTree.cpp
        Trigger/ratesLumiFit/OHltTree.h
        Trigger/ratesLumiFit/OHltTreeOpen.cpp
        Trigger/ratesLumiFit/getSlopesInterceptsMultRunsMultDS.txt
        Trigger/ratesLumiFit/hltmenu_ratesLumiFit.cfg.template
        Trigger/ratesLumiFit/multGetIndividualPerSlices.txt
        Trigger/ratesLumiFit/multipleOHlt.sh
        Trigger/ratesLumiFit/root526.sh
        crab/crab.cfg
        crab/template_TTbar_7TeV_cfi_py_GEN_FASTSIM_cfg.py
        myTemplateAnalysis/bin/BuildFile
        myTemplateAnalysis/bin/myTemplateAnalysis.cc
        myTemplateAnalysis/interface/myTemplateAnalyzer.h
        myTemplateAnalysis/interface/myTemplateHistograms.h
        plugins/EDFilterAnalyzer.cc
        plugins/EDFilterAnalyzer.h
        python/corCaloMet_cfi.py

--- python/corCaloMet_cfi.py
+++
@@ -1,6 +0,0 @@
-from JetMETCorrections.Configuration.JetCorrectionServicesAllAlgos_cff import *
-from JetMETCorrections.Type1MET.MetType1Corrections_cff import *
-
-metMuonJESCorAK5 = metJESCorAK5CaloJet.clone()
-metMuonJESCorAK5.inputUncorMetLabel = "corMetGlobalMuons"
-metCorSequence = cms.Sequence(metMuonJESCorAK5)
