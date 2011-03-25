7ae08443b3bc1efa50d4bdca4edba4db6cd40b50
lucieg <>
2011-03-25 19:38:16 +0000

    *** empty log message ***

--- METsWithPU/METsAnalyzer/python/pfMetNoPileUp_cff.py
+++ METsWithPU/METsAnalyzer/python/pfMetNoPileUp_cff.py
@@ -3,5 +3,20 @@ import FWCore.ParameterSet.Config as cms
 from METsWithPU.METsAnalyzer.pfMET_cfi import *
 from CommonTools.ParticleFlow.pfNoPileUp_cff import *
 
-pfMetNoPileUp = pfMET.clone()
-pfMetNoPileUp.src = 'pfNoPileUp'
+#produce pf pile up candidates with offlinePrimaryVerticesDA
+pfPileUpOldVtces = pfPileUp.clone()
+pfPileUpOldVtces.Vertices = cms.InputTag("offlinePrimaryVertices")
+
+#replace the top collection used by pfNoPileUp 
+pfNoPileUpOldVtces = pfNoPileUp.clone()
+pfNoPileUpOldVtces.topCollection = 'pfPileUpOldVtces'
+
+pfNoPileUpOldVtcesSequence = cms.Sequence(
+    pfPileUpOldVtces +
+    pfNoPileUpOldVtces 
+    )
+
+#produce pfMetNoPileUp with the pfNoPileUpDA 
+pfMetNoPileUp     = pfMET.clone()
+pfMetNoPileUp.src = 'pfNoPileUpOldVtces'
+
