5fca7ca8b92ee007f50c3280587b29168017842c
lucieg <>
2011-07-14 09:11:51 +0000

    added a cleaner version of effPlots - does less though

---
+++ macros/options.py
@@ -0,0 +1,23 @@
+
+
+
+#VBTF ID
+cutsVBTFID                   = ["","cuts_vbtf95ID", "cuts_vbtf90ID", "cuts_vbtf80ID", "cuts_vbtf70ID", "cuts_vbtf60ID"]
+
+
+
+#VBTF CR
+cutsVBTFCR                   = ["","cuts_vbtf95CR", "cuts_vbtf90CR", "cuts_vbtf80CR", "cuts_vbtf70CR", "cuts_vbtf60CR"]
+
+
+#CiC ID
+cutsCiCID                    = ["", "cuts_veryLooseID", "cuts_looseID", "cuts_mediumID", "cuts_tightID", "cuts_superTightID"]
+
+
+#CiC CR
+cutsCiCCR                    = ["", "cuts_veryLooseCR", "cuts_looseCR", "cuts_mediumCR", "cuts_tightCR", "cuts_superTightCR"]
+
+
+#Isol
+cutsIsol                     = [1000., 1., 0.7, 0.5, 0.4, 0.3, 0.2, 0.1]
+
