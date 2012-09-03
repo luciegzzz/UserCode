72d1ea1791524dd77fac3de9841dd9a8b9420d9e
lucieg <>
2012-09-03 14:20:30 +0000

    1st commit

---
+++ T1tttt/prod/TTbar_Tauola_8TeV_cfi_py_GEN_FASTSIM_HLT_NoPU.py
@@ -0,0 +1,136 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.372.2.3 
+# Source: /local/reps/CMSSW.admin/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: TTbar_Tauola_8TeV_cfi.py -s GEN,FASTSIM,HLT:GRun --pileup=2012_Startup_inTimeOnly --geometry DB --conditions=auto:startup --eventcontent=AODSIM --datatier GEN-SIM-DIGI-RECO -n 10 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('HLT')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('FastSimulation.Configuration.EventContent_cff')
+process.load('FastSimulation.PileUpProducer.PileUpSimulator_NoPileUp_cff')
+process.load('FastSimulation.Configuration.Geometries_START_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('FastSimulation.Configuration.FamosSequences_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedParameters_cfi')
+process.load('HLTrigger.Configuration.HLT_GRun_Famos_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10000)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.372.2.3 $'),
+    annotation = cms.untracked.string('TTbar_Tauola_8TeV_cfi.py nevts:100'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.AODSIMoutput = cms.OutputModule("PoolOutputModule",
+    eventAutoFlushCompressedSize = cms.untracked.int32(15728640),
+    outputCommands = process.AODSIMEventContent.outputCommands,
+   # fileName = cms.untracked.string('/data/lucieg/boostedTops/NoPU/TTbar_Tauola_8TeV_cfi_py_GEN_FASTSIM_HLT_NoPU_900_1000GeV.root'),
+  #  fileName = cms.untracked.string('/data/lucieg/boostedTops/NoPU/TTbar_Tauola_8TeV_cfi_py_GEN_FASTSIM_HLT_NoPU_300_400GeV.root'),
+    fileName = cms.untracked.string('/data/lucieg/boostedTops/NoPU/TTbar_Tauola_8TeV_cfi_py_GEN_FASTSIM_HLT_NoPU_600_700GeV_10K.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM-DIGI-RECO')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+# Additional output definition
+
+# Other statements
+process.famosSimHits.SimulateCalorimetry = True
+process.famosSimHits.SimulateTracking = True
+process.simulation = cms.Sequence(process.simulationWithFamos)
+process.HLTEndSequence = cms.Sequence(process.reconstructionWithFamos)
+process.Realistic8TeVCollisionVtxSmearingParameters.type = cms.string("BetaFunc")
+process.famosSimHits.VertexGenerator = process.Realistic8TeVCollisionVtxSmearingParameters
+process.famosPileUp.VertexGenerator = process.Realistic8TeVCollisionVtxSmearingParameters
+process.GlobalTag.globaltag = 'START52_V9::All'
+
+process.generator = cms.EDFilter("Pythia6GeneratorFilter",
+    ExternalDecays = cms.PSet(
+        Tauola = cms.untracked.PSet(
+            UseTauolaPolarization = cms.bool(True),
+            InputCards = cms.PSet(
+                mdtau = cms.int32(0),
+                pjak2 = cms.int32(0),
+                pjak1 = cms.int32(0)
+            )
+        ),
+        parameterSets = cms.vstring('Tauola')
+    ),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(8000.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    PythiaParameters = cms.PSet(
+        pythiaUESettings = cms.vstring('MSTU(21)=1     ! Check on possible errors during program execution', 
+            'MSTJ(22)=2     ! Decay those unstable particles', 
+            'PARJ(71)=10 .  ! for which ctau  10 mm', 
+            'MSTP(33)=0     ! no K factors in hard cross sections', 
+            'MSTP(2)=1      ! which order running alphaS', 
+            'MSTP(51)=10042 ! structure function chosen (external PDF CTEQ6L1)', 
+            'MSTP(52)=2     ! work with LHAPDF', 
+            'PARP(82)=1.921 ! pt cutoff for multiparton interactions', 
+            'PARP(89)=1800. ! sqrts for which PARP82 is set', 
+            'PARP(90)=0.227 ! Multiple interactions: rescaling power', 
+            'MSTP(95)=6     ! CR (color reconnection parameters)', 
+            'PARP(77)=1.016 ! CR', 
+            'PARP(78)=0.538 ! CR', 
+            'PARP(80)=0.1   ! Prob. colored parton from BBR', 
+            'PARP(83)=0.356 ! Multiple interactions: matter distribution parameter', 
+            'PARP(84)=0.651 ! Multiple interactions: matter distribution parameter', 
+            'PARP(62)=1.025 ! ISR cutoff', 
+            'MSTP(91)=1     ! Gaussian primordial kT', 
+            'PARP(93)=10.0  ! primordial kT-max', 
+            'MSTP(81)=21    ! multiple parton interactions 1 is Pythia default', 
+            'MSTP(82)=4     ! Defines the multi-parton model'),
+        processParameters = cms.vstring('MSEL      = 0     ! User defined processes', 
+            'MSUB(81)  = 1     ! qqbar to QQbar', 
+            'MSUB(82)  = 1     ! gg to QQbar', 
+            'MSTP(7)   = 6     ! flavour = top', 
+            'PMAS(6,1) = 175.  ! top quark mass',
+	    'CKIN(3)=600.        ! minimum pt hat for hard interactions', ##change this to vary the boost
+            'CKIN(4)=700.        ! maximum pt hat for hard interactions'),
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen_genonly)
+process.reconstruction = cms.Path(process.reconstructionWithFamos)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.AODSIMoutput_step = cms.EndPath(process.AODSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step)
+process.schedule.extend(process.HLTSchedule)
+process.schedule.extend([process.reconstruction,process.AODSIMoutput_step])
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
+
